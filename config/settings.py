74f29ce7fe2e1524bd7b22d6d056cc67b5dc9899
ohnas <74810513+ohnas@users.noreply.github.com>
2021-08-08 16:44:53 +0900

    #3 Set up Upload photos

--- config/settings.py
+++ config/settings.py
@@ -51,13 +51,13 @@ PROJECT_APPS = [
     "reviews.apps.ReviewsConfig",
 ]
 
-"""
+
 THIRD_PARTY_APPS = [
     "django_seed",
 ]
-"""
 
-INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
+
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
